8b530881bb32f87b7193f2d869f61debf1174421
Sergi <servenord@gmail.com>
2025-06-08 18:58:59 +0200

    Sol·lució d'errors amb l'alineació dels botons

--- script.js
+++ script.js
@@ -19,13 +19,15 @@ function mostrarMissioMatematiques() {
   let operacions = ["4x6", "3x8", "2x12", "5x5"];
 
   let html = `<h2>Missió: Matemàtiques</h2>
-              <p>${pregunta}</p>`;
+              <p>${pregunta}</p>
+              <div class="botons-missio">`;
 
   operacions.forEach(op => {
-    html += `<button onclick="comprovarResposta('${op}')">${op}</button> `;
+    html += `<button onclick="comprovarResposta('${op}')">${op}</button>`;
   });
 
-  html += `<br><br><button onclick="tornarAlMenu()">Tornar al menú</button>`;
+  html += `</div>
+           <br><button onclick="tornarAlMenu()">Tornar al menú</button>`;
 
   document.getElementById('joc').innerHTML = html;
 }
@@ -35,13 +37,15 @@ function mostrarMissioLlengua() {
   let respostes = ["Quan acaben en vocal", "Quan acaben en vocal, vocal + s o vocal + n", "Quan acaben en vocal, vocal + n, -es o -is", "Quan acaben en vocal, vocal + s, -en o -in"];
 
   let html = `<h2>Missió: Llengua</h2>
-              <p>${pregunta}</p>`;
+              <p>${pregunta}</p>
+              <div class="botons-missio">`;
 
-  respostes.forEach(op => {
-    html += `<button onclick="comprovarResposta('${op}')">${op}</button> `;
+  operacions.forEach(op => {
+    html += `<button onclick="comprovarResposta('${op}')">${op}</button>`;
   });
 
-  html += `<br><br><button onclick="tornarAlMenu()">Tornar al menú</button>`;
+  html += `</div>
+           <br><button onclick="tornarAlMenu()">Tornar al menú</button>`;
 
   document.getElementById('joc').innerHTML = html;
 }
