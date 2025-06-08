a71df87cd2114f39502696d233508dff47fd89df
Sergi <servenord@gmail.com>
2025-06-08 18:11:42 +0200

    Afegida d'un missió de llengua i canvi de l'estil dels botons

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function iniciarJoc(missio) {
   document.getElementById('menu').style.display = 'none';
   document.getElementById('joc').style.display = 'block';
 
-  if (missio === 'matematiques') {
+  if (missio === 'matemàtiques') {
     mostrarMissioMatematiques();
-  } else {
-    document.getElementById('joc').innerHTML = `<p>Missió de ${missio} encara no disponible.</p>
+  } 
+  else if (missio === 'llengua') {
+    mostrarMissioLlengua();
+  } 
+  else {
+    document.getElementById('joc').innerHTML = `<p>Les missions de ${missio} encara no estan disponibles.</p>
     <button onclick="tornarAlMenu()">Tornar al menú</button>`;
   }
 }
@@ -26,8 +30,24 @@ function mostrarMissioMatematiques() {
   document.getElementById('joc').innerHTML = html;
 }
 
+function mostrarMissioLlengua() {
+  let pregunta = "Quan s'accentuen les paraules agudes?";
+  let respostes = ["Quan acaben en vocal", "Quan acaben en vocal, vocal + s o vocal + n", "Quan acaben en vocal, vocal + n, -es o -is", "Quan acaben en vocal, vocal + s, -en o -in"];
+
+  let html = `<h2>Missió: Llengua</h2>
+              <p>${pregunta}</p>`;
+
+  respostes.forEach(op => {
+    html += `<button onclick="comprovarResposta('${op}')">${op}</button> `;
+  });
+
+  html += `<br><br><button onclick="tornarAlMenu()">Tornar al menú</button>`;
+
+  document.getElementById('joc').innerHTML = html;
+}
+
 function comprovarResposta(opcio) {
-  let correctes = ["4x6", "3x8", "2x12"]; // Les respostes correctes
+  let correctes = ["Quan acaben en vocal, vocal + s, -en o -in", "4x6", "3x8", "2x12"]; // Les respostes correctes
   if (correctes.includes(opcio)) {
     alert("Correcte! 🎉");
   } else {
